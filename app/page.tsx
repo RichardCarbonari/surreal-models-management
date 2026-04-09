03aeb644d1a8360c7ac3a7f2c1348418e78332ca
Stevie Jones <steviejones@MacBook-Air-de-Stevie.local>
2026-04-10 00:16:41 +0530

    fix: remove models import from home page, HomeClient uses internal images

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 export const dynamic = "force-dynamic";
-import { models } from "@/data/models";
 import HomeClient from "@/components/HomeClient";
 
 export default function HomePage() {
-  const images = [...models, ...models, ...models];
-  return <HomeClient images={images} />;
+  return <HomeClient images={[]} />;
 }
