636d180ccd0fb083df041f42ff00e18bf16d00de
Stevie Jones <steviejones@MacBook-Air-de-Stevie.local>
2026-05-04 16:12:52 +0530

    feat: add João Pauda — 11 photos

--- data/models.ts
+++ data/models.ts
@@ -255,6 +255,14 @@ export const models: Model[] = [
     instagram: "https://www.instagram.com/jassikamuzu/",
     portfolio: "https://drive.google.com/drive/folders/1qjtQXY1czSPN3UdW9Rhq1pu8-Qs842nx",
   },
+  {
+    id: "27", slug: "joao-pauda", name: "João Pauda", category: "masculino",
+    coverImage: "https://lh3.googleusercontent.com/d/1eJHlG0M7V_2k5Z0fx2gr-txsZcg-d0ca",
+    images: ["https://lh3.googleusercontent.com/d/1DWAXlrrdwyscdYwGoRQpql-6w0RaLIcx","https://lh3.googleusercontent.com/d/1NIbppIq7sjlPv0GYo6V8ZAoKndknHuez","https://lh3.googleusercontent.com/d/1SBBy9Pu4y1bDKUUFbQbz8c2FyDRqc71m","https://lh3.googleusercontent.com/d/1URYy8ezzt4yqR652PyUZLQEvyIdgYePu","https://lh3.googleusercontent.com/d/1VDTPRvz1lmWNtmnLrAplusJ5XEn3o91t","https://lh3.googleusercontent.com/d/1XOVIL4JrBI3EJUGsGiJx9nTqSHygaZBW","https://lh3.googleusercontent.com/d/1XZqWnxCDIVSQ8-dZlOUsggo18bkhhPWZ","https://lh3.googleusercontent.com/d/1eJHlG0M7V_2k5Z0fx2gr-txsZcg-d0ca","https://lh3.googleusercontent.com/d/1sK1mnFm2ofWkRzQxfGEmxFMqZsGB3rxm","https://lh3.googleusercontent.com/d/1vKV_Dhlf6_HiaouDCayjp-QBJU_L5FyP","https://lh3.googleusercontent.com/d/1zHOrxc-iUD6R1_k2jUVH4zWc9lJ6YNzo"],
+    polaroids: [], height: "", bust: "", waist: "", hips: "",
+    shoes: "", shirts: "", pants: "",
+    eyes: "", hair: "", nationality: "Brasileiro",
+  },
 ];
 
 export const getFeaturedModels = () => models.filter((m) => m.featured);
